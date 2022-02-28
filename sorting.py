185ed74ee4fb5301046f3a2f1bd1d59b65bc1f42
Tengku Syaid Rian Fadhilah <49834864+rianfadhilah@users.noreply.github.com>
2022-02-28 16:57:53 +0700

    Delete sorting.py

--- sorting.py
+++
@@ -1,5 +0,0 @@
-import pandas as py
-ujian = py.read_csv("SkorUjian.csv")
-print(ujian.sort_values(by = 'math score' , ascending = False).head())
-
-print(ujian["math score"].sort_values(ascending = False)[:5])
